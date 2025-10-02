9bcb97f969222e84c1c6abe55518f344f0e2a754
milashinis <milashinis@gmail.com>
2025-10-02 15:40:53 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Streamlit App", layout="wide")
+
+st.title("🚀 Hello, Streamlit!")
+st.write("If you see this, deployment worked successfully.")
